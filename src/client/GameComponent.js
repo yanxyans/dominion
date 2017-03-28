e5ff6f42a0baedfdd7035563e43b41dadac39db6
Jason Yan <fssyan@gmail.com>
2017-03-28 17:32:15 -0400

    display card with chip

--- src/client/GameComponent.js
+++ src/client/GameComponent.js
@@ -3,6 +3,19 @@ import MobileTearSheet from './MobileTearSheet';
 import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table';
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import Chip from 'material-ui/Chip';
+
+const styles = {
+  chip: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+		justifyContent: 'center'
+  },
+};
 
 export default class Container extends React.Component {
 	
@@ -33,9 +46,7 @@ export default class Container extends React.Component {
 		let ActionList = this.props.action ? (
 			<div id='action'>
 				<Divider />
-				<List>
-					<ListItem primaryText={this.props.actionName} onTouchTap={this.handleAction} />
-				</List>
+				<ListItem primaryText={this.props.actionName} onTouchTap={this.handleAction} />
 			</div>
 		) : null;
 		let PlayerTable = this.props.player ? (
@@ -43,70 +54,56 @@ export default class Container extends React.Component {
 				<List>
 					<ListItem primaryText={'name = ' + this.props.player.name} />
 					<ListItem primaryText={'deck size = ' + this.props.player.deckSize} />
-					<ListItem primaryText={'action = ' + this.props.player.resource.action} />
-					<ListItem primaryText={'buy = ' + this.props.player.resource.buy} />
-					<ListItem primaryText={'coin = ' + this.props.player.resource.coin} />
-					<ListItem primaryText={'potion = ' + this.props.player.resource.potion} />
+					<ListItem primaryText={'a=' + this.props.player.resource.action +
+																 ' b=' + this.props.player.resource.buy +
+																 ' c=' + this.props.player.resource.coin +
+																 ' p=' + this.props.player.resource.potion} />
+				
+					<Divider />
+					<Subheader>Discard</Subheader>
+					<div id='discard' style={styles.wrapper}>
+					{this.props.player.discard.map(function(card, index) {
+						return <Chip key={index} style={styles.chip}>{card}</Chip>;
+					})}
+					</div>
+					<Divider />
+					<Subheader>Played</Subheader>
+					<div id='in_play' style={styles.wrapper}>
+					{this.props.player.inPlay.map(function(card, index) {
+						return <Chip key={index} style={styles.chip}>{card}</Chip>;
+					})}
+					</div>
+					<Divider />
+					<Subheader>Hand</Subheader>
+					<div id='in_hand' style={styles.wrapper}>
+					{this.props.player.hand.map(function(card, index) {
+						return <Chip key={index} style={styles.chip}>{card}</Chip>;
+					})}
+					</div>
+					{ActionList}
 				</List>
-				<Divider />
-				<Table onRowSelection={this.handleDiscard}>
-					<TableBody>
-						{this.props.player.discard.map(function(card, index) {
-							return <TableRow key={index}>
-									     <TableRowColumn>{card}</TableRowColumn>
-								     </TableRow>;
-						})}
-					</TableBody>
-				</Table>
-				<Divider inset={true} />
-				<Table onRowSelection={this.handlePlay}>
-					<TableBody>
-						{this.props.player.inPlay.map(function(card, index) {
-							return <TableRow key={index}>
-									     <TableRowColumn>{card}</TableRowColumn>
-								     </TableRow>;
-						})}
-					</TableBody>
-				</Table>
-				<Divider inset={true} />
-				<Table onRowSelection={this.handleHand}>
-					<TableBody>
-						{this.props.player.hand.map(function(card, index) {
-							return <TableRow key={index}>
-										   <TableRowColumn>{card}</TableRowColumn>
-										 </TableRow>;
-						})}
-					</TableBody>
-				</Table>
-				{ActionList}
 			</MobileTearSheet>
 		) : null;
-		
-		let PlayerTables = this.props.players ? (
+		let PlayerTables = PlayerTable || this.props.players ? (
 			<div id='players'>
+				{PlayerTable}
 				{this.props.players.map(function(player, index) {
 					return <MobileTearSheet key={index}>
 								   <List>
 										 <ListItem primaryText={'name = ' + player.name} />
 										 <ListItem primaryText={'deck size = ' + player.deckSize} />
-										 <ListItem primaryText={'discard top = ' + player.discardTop ?
-										   player.discardTop[0] : null} />
+										 <ListItem primaryText={'discard top = ' + (player.discardTop[0] ? player.discardTop[0] : 'none')} />
 										 <ListItem primaryText={'hand size = ' + player.handSize} />
-										 <ListItem primaryText={'action = ' + player.resource.action} />
-										 <ListItem primaryText={'buy = ' + player.resource.buy} />
-										 <ListItem primaryText={'coin = ' + player.resource.coin} />
-										 <ListItem primaryText={'potion = ' + player.resource.potion} />
+										 <ListItem primaryText={'a=' + player.resource.action +
+																						' b=' + player.resource.buy +
+																						' c=' + player.resource.coin +
+																						' p=' + player.resource.potion} />
+										 <Divider />
+										 <Subheader>Played</Subheader>
+										 {player.inPlay.map(function(card, index) {
+											 return <Chip key={index}>{card}</Chip>;
+										 })}
 									 </List>
-									 <Divider />
-									 <Table>
-									   <TableBody>
-										   {player.inPlay.map(function(card, index) {
-											   return <TableRow key={index}>
-														      <TableRowColumn>{card}</TableRowColumn>
-													      </TableRow>;
-											 })}
-									   </TableBody>
-									 </Table>
 								 </MobileTearSheet>;
 				})}
 			</div>
@@ -142,12 +139,16 @@ export default class Container extends React.Component {
 				</Table>
 			</MobileTearSheet>
 		) : null;
-    return (
-			<div id='game'>
-				{PlayerTable}
-				{PlayerTables}
+		let GameTable = PileTable || UserTable ? (
+			<div id='gameBoard'>
 				{PileTable}
 				{UserTable}
+			</div>
+		) : null;
+    return (
+			<div id='game'>
+				{PlayerTables}
+				{GameTable}
 			</div>
 		)
   }
