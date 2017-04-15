a7a40d87e483dfc6dfd987b9e3760ad2983b1ac7
Jason Yan <fssyan@gmail.com>
2017-04-15 16:20:51 -0400

    add poop

--- src/client/MenuComponent.js
+++ src/client/MenuComponent.js
@@ -9,18 +9,25 @@ import SelectableList from './SelectableList';
 export default class MenuComponent extends React.Component {
   render() {
     return (
-			<MobileTearSheet>
-				<List>
-					<NameComponent name={this.props.name} _setName={this.props._setName} />
-					<JoinComponent _joinGame={this.props._joinGame} />
-				</List>
-				<Divider />
-				<SelectableList inRoom={this.props.inRoom} _enterGame={this.props._enterGame}>
-					{Object.keys(this.props.rooms).map(function(room) {
-						return <ListItem key={room} value={room}>{room}</ListItem>;
-					})}
-				</SelectableList>
-			</MobileTearSheet>
+			<div id='menu'>
+				<MobileTearSheet id='menu_child'>
+					<List>
+						<NameComponent name={this.props.name} _setName={this.props._setName} />
+						<JoinComponent _joinGame={this.props._joinGame} />
+					</List>
+					<Divider />
+					<SelectableList inRoom={this.props.inRoom} _enterGame={this.props._enterGame}>
+						{Object.keys(this.props.rooms).map(function(room) {
+							return <ListItem key={room} value={room}>{room}</ListItem>;
+						})}
+					</SelectableList>
+				</MobileTearSheet>
+				<MobileTearSheet>
+					<List>
+						<ListItem primaryText="poop" />
+					</List>
+				</MobileTearSheet>
+			</div>
     )
   }
 };
