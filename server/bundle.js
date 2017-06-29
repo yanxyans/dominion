9694d389ea43c0a6b9a37805c567dabebc2ccffe
Jason Yan <fssyan@gmail.com>
2017-06-29 12:40:17 -0400

    fixed bundle spacing

--- server/bundle.js
+++ server/bundle.js
@@ -7,46 +7,46 @@ var mainPath = path.resolve(__dirname, '..', 'app', 'Container');
 
 module.exports = function () {
 
-  // First we fire up Webpack and pass in the configuration we
-  // created
-  var bundleStart = null;
-  var compiler = Webpack(webpackConfig);
+    // First we fire up Webpack and pass in the configuration we
+    // created
+    var bundleStart = null;
+    var compiler = Webpack(webpackConfig);
 
-  // We give notice in the terminal when it starts bundling and
-  // set the time it started
-  compiler.plugin('compile', function() {
-    console.log('Bundling...');
-    bundleStart = Date.now();
-  });
+    // We give notice in the terminal when it starts bundling and
+    // set the time it started
+    compiler.plugin('compile', function() {
+        console.log('Bundling...');
+        bundleStart = Date.now();
+    });
 
-  // We also give notice when it is done compiling, including the
-  // time it took. Nice to have
-  compiler.plugin('done', function() {
-    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
-  });
+    // We also give notice when it is done compiling, including the
+    // time it took. Nice to have
+    compiler.plugin('done', function() {
+        console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
+    });
 
-  var bundler = new WebpackDevServer(compiler, {
+    var bundler = new WebpackDevServer(compiler, {
 
-    // We need to tell Webpack to serve our bundled application
-    // from the build path. When proxying:
-    // http://localhost:3000/build -> http://localhost:8080/build
-    publicPath: '/build/',
+        // We need to tell Webpack to serve our bundled application
+        // from the build path. When proxying:
+        // http://localhost:3000/build -> http://localhost:8080/build
+        publicPath: '/build/',
 
-    // Configure hot replacement
-    hot: true,
+        // Configure hot replacement
+        hot: true,
 
-    // The rest is terminal configurations
-    quiet: false,
-    noInfo: true,
-    stats: {
-      colors: true
-    }
-  });
+        // The rest is terminal configurations
+        quiet: false,
+        noInfo: true,
+        stats: {
+          colors: true
+        }
+    });
 
-  // We fire up the development server and give notice in the terminal
-  // that we are starting the initial bundle
-  bundler.listen(8080, 'localhost', function () {
-    console.log('Bundling project, please wait...');
-  });
+    // We fire up the development server and give notice in the terminal
+    // that we are starting the initial bundle
+    bundler.listen(8080, 'localhost', function () {
+        console.log('Bundling project, please wait...');
+    });
 
 };
