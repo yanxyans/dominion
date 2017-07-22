b76077f559f848d4f19b58bddefb60d07642e802
Jason Yan <fssyan@gmail.com>
2017-07-22 04:13:29 -0400

    control buttons

---
+++ app/ControlButtons.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const audio = new Audio('/asset/notif.mp3');
+
+export default class ControlButtons extends React.Component {
+    
+    componentDidMount() {
+        var player = this.props.player;
+        var peek = player.control[0];
+        if (player.isPlayer &&
+            peek !== 'Start' &&
+            peek !== 'Restart') {
+            audio.play();
+        }
+    }
+    
+    render() {
+        var player = this.props.player;
+        var _complete = this.props._complete;
+        
+        return (
+            <div className='buttons'>
+                {player.control.map(function(comm, index) {
+                    return <RaisedButton key={index}
+                                         label={(comm === 'Action' && player.action + ' action') ||
+                                                (comm === 'Buy' && player.buy + ' buy ' + player.coin + ' coin') ||
+                                                comm}
+                                         primary={player.main && player.phase - 1 === index}
+                                         onTouchTap={_complete.bind(null, comm)}/>;
+                })}
+            </div>
+        );
+    }
+}
--- app/PlayerComponent.js
+++ app/PlayerComponent.js
@@ -1,79 +1,36 @@
 import React from 'react';
 
 import Paper from 'material-ui/Paper';
-import IconButton from 'material-ui/IconButton';
-import IconName from 'material-ui/svg-icons/action/face';
-import IconDisconnect from 'material-ui/svg-icons/alert/warning';
-import IconPoints from 'material-ui/svg-icons/action/grade'
-import IconOne from 'material-ui/svg-icons/image/looks-one';
-import IconTwo from 'material-ui/svg-icons/image/looks-two';
-import IconThree from 'material-ui/svg-icons/image/looks-3';
-import IconFour from 'material-ui/svg-icons/image/looks-4';
+import RaisedButton from 'material-ui/RaisedButton';
 
-import ControlComponent from './ControlComponent';
 import Stack from './Stack';
-
-const styles = {
-    icon: {
-        fontSize: '20px',
-        width: 'auto',
-        color: '#fff'
-    }
-}
-
-function plural(attr) {
-    return attr === 1 ? '' : 's';
-}
+import ControlButtons from './ControlButtons';
 
 export default class PlayerComponent extends React.Component {
-    
+
     render() {
         var player = this.props.player;
         var source = ['players', player.slot];
         var tooltip = this.props.tooltip;
+        var _complete = this.props._complete;
         var _tap = this.props._tap;
-
+        
         return (
-            <div className={player.isTurn ? 'player' : 'player inactive'}>
-                <Paper className='title' zDepth={1} style={{minHeight:'82px',backgroundColor:this.props.color}}>
-                    <div className='buttons'>
-                        <IconButton tooltip={player.name}
-                                    onTouchTap={this.props._recon}>
-                            {player.disc ?
-                                <IconDisconnect/> :
-                                <IconName/>}
-                        </IconButton>
+            <div className='player'>
+                <Paper className={player.isTurn ? 'title' : 'title inactive'}
+                       zDepth={1}
+                       style={{backgroundColor:this.props.color, padding:'5px'}}>
+                    <RaisedButton label={player.name}
+                                  primary={!player.disc}
+                                  onTouchTap={this.props._recon}/>
                         
-                        {false &&
-                        <IconButton tooltip={player.points + ' point' + plural(player.points)}>
-                            {player.rank === 0 && <IconOne/>}
-                            {player.rank === 1 && <IconTwo/>}
-                            {player.rank === 2 && <IconThree/>}
-                            {player.rank === 3 && <IconFour/>}
-                        </IconButton>}
-                        
-                        {false &&
-                        <IconButton tooltip={player.action + ' action' + plural(player.action)}
-                                    style={styles.icon}>
-                            {player.action + 'A'}
-                        </IconButton>}
-                        {false &&
-                        <IconButton tooltip={player.buy + ' buy' + plural(player.buy)}
-                                    style={styles.icon}>
-                            {player.buy + 'B'}
-                        </IconButton>}
-                        {false &&
-                        <IconButton tooltip={player.coin + ' coin' + plural(player.coin)}
-                                    style={styles.icon}>
-                            {player.coin + 'C'}
-                        </IconButton>}
-                    </div>
+                    {player.rank !== -1 &&
+                    <RaisedButton label={player.points + ' vp'}
+                                  primary={true}/>}
                     
-                    {false &&
-                    <ControlComponent control={player.control}
-                                      phase={player.main ? player.phase - 1 : null}
-                                      isPlayer={player.isPlayer}
-                                      _complete={this.props._complete}/>}
+                    {player.control &&
+                    <ControlButtons player={player}
+                                    _complete={_complete}/>}
                 </Paper>
                 
                 {!this.props.hideContent &&
