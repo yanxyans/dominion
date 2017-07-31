7b81917c35a3313ce3bfcc658bfa8f3df0ee0033
Jason Yan <fssyan@gmail.com>
2017-07-30 22:04:59 -0400

    moved vp to toolbar title

--- app/Player.js
+++ app/Player.js
@@ -11,6 +11,12 @@ export default class Player extends React.Component {
 
     render() {
         var player = this.props.player;
+        var name = player.name;
+        if (player.rank !== -1) {
+            name += '/';
+            name += player.points;
+            name += 'vp';
+        }
         var source = ['players', player.slot];
         var tooltip = this.props.tooltip;
         
@@ -20,16 +26,13 @@ export default class Player extends React.Component {
         return (
             <div className={player.isTurn ? 'player' : 'player inactive'}>
                 <Toolbar className='title'
-                         style={{backgroundColor:this.props.color, zIndex:1}}>
+                         style={{backgroundColor:this.props.color}}>
                     <ToolbarGroup>
-                        <ToolbarTitle text={player.name}
+                        <ToolbarTitle text={name}
                                       onTouchTap={this.props._recon}
                                       style={{color:player.disc ? '#e0e0e0' : '#303030'}}/>
                     </ToolbarGroup>
                     <ToolbarGroup>
-                        {player.rank !== -1 &&
-                        <RaisedButton label={player.points + ' vp'}/>}
-                        
                         {player.control &&
                         <Control player={player}
                                  _complete={_complete}/>}
