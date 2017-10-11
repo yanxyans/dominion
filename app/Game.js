71a2839a12575ab050cf61916ce5240201709ce6
Jason Yan <fssyan@gmail.com>
2017-10-11 03:36:51 -0400

    swapped player and board

--- app/Game.js
+++ app/Game.js
@@ -9,16 +9,16 @@ export default class GameComponent extends React.Component {
     render() {
         return (
             <FullWidthSection id='game'>
-                <Board supply={this.props.supply}
-                       trash={this.props.trash}
-                       tooltip={this.props.tooltip}
-                       _tap={this.props._tap}/>
                 <PlayerAll isPlayer={this.props.isPlayer}
                            players={this.props.players}
                            tooltip={this.props.tooltip}
                            _recon={this.props._recon}
                            _complete={this.props._complete}
                            _tap={this.props._tap}/>
+                <Board supply={this.props.supply}
+                       trash={this.props.trash}
+                       tooltip={this.props.tooltip}
+                       _tap={this.props._tap}/>
             </FullWidthSection>
         );
     }
